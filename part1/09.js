1ccbdf5f7406e9aef95e62c7f8b5d02029a5ba83
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2023-03-11 12:34:24 +0530

    Update 09.js

--- part1/09.js
+++ part1/09.js
@@ -3,5 +3,5 @@
 let string1 = "17";
 let string2 = "10";
 
-let newString = +string1 + +string2;
+let newString = +string1 + " " +string2;   //" " bich me space ho gaya 
 console.log(typeof newString);
