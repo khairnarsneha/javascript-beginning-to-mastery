33edd7b1f2e6f2f54061b8d9ea28ca800bee08e4
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2023-03-11 12:38:42 +0530

    Update 09.js

--- part1/09.js
+++ part1/09.js
@@ -1,7 +1,14 @@
 // string concatenation 
 
+let string1 = "harshit";
+let string2 = "vashisht";
+
+let fullName = +string1 + " " +string2;   //" " bich me space ho gaya 
+console.log(typeof fullName);
+
+
 let string1 = "17";
 let string2 = "10";
 
-let newString = +string1 + " " +string2;   //" " bich me space ho gaya 
+let newString = +string1 + +string2;   // 27
 console.log(typeof newString);
