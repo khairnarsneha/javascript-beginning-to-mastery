2681b65bd2e87cab2684359021aa7118a9ebfd34
Sneha Hari Khairnar <109480526+khairnarsneha@users.noreply.github.com>
2023-04-28 16:52:39 +0530

    Update 04.js

--- part1/04.js
+++ part1/04.js
@@ -1,6 +1,12 @@
 // let keyword 
 // declare variable with let keyword 
+//let keyword sirf ekbaar hi used kar sakte hai same variable name ke liye 
+//wrong code 
+let firstName = "harshit";
+let firstName = "Mohit";
+console.log(firstName);
 
+//right code
 let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
